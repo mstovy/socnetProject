307372dc4e1408d5c90fb866a2353fe60611dc47
Micah Stovall <micahstovall@Micahs-MacBook-Pro-2.local>
2020-11-05 20:01:49 -0700

    upload #2

---
+++ Node.py
@@ -0,0 +1,8 @@
+class NodeClass:
+    idV = 0
+    x_coord = 0
+    y_coord = 0
+    def __init__(self, idV, x_coord, y_coord):
+        self.id V= idV
+        self.x_coord = x_coord
+        self.y_coord = y_coord
---
+++ NodeClass.py
@@ -0,0 +1,8 @@
+class NodeClass:
+    # id = 0
+    #x_coord = 0
+    #y_coord = 0
+    def __init__(self, idV, x_coord, y_coord):
+        self.idV = idV
+        self.x_coord = x_coord
+        self.y_coord = y_coord
