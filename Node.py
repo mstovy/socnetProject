69db1cbfb01225cb8f72a2c70ed6e574435385e9
Henry Tan <henrytan508@gmail.com>
2020-11-09 20:33:23 -0700

    removing non us nodes

--- Node.py
+++ Node.py
@@ -1,8 +1,5 @@
-class NodeClass:
-    idV = 0
-    x_coord = 0
-    y_coord = 0
-    def __init__(self, idV, x_coord, y_coord):
-        self.id V= idV
-        self.x_coord = x_coord
-        self.y_coord = y_coord
+class Node:
+    def __init__(self, user_id, latitude, longitude):
+        self.user_id = user_id
+        self.latitude = latitude
+        self.longitude = longitude
--- NodeClass.py
+++
@@ -1,8 +0,0 @@
-class NodeClass:
-    # id = 0
-    #x_coord = 0
-    #y_coord = 0
-    def __init__(self, idV, x_coord, y_coord):
-        self.idV = idV
-        self.x_coord = x_coord
-        self.y_coord = y_coord
