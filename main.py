a93c7fae5befafc3fd0d69ce03acdf6e6c9756a4
Henry Tan <henrytan508@gmail.com>
2020-11-12 17:51:53 -0700

    gowalla edge file missing

--- main.py
+++ main.py
@@ -5,10 +5,15 @@ from mpl_toolkits.basemap import Basemap
 path = os.path.dirname(__file__)
 checkin_file_path = os.path.join(path, "gowalla_checkins_average.csv")
 df = pd.read_csv(checkin_file_path)
+edges_file_path = os.path.join(path, "Gowalla_edges.txt")
+graph = nx.Graph()
 
 def create_nodes():
-    
+    for index, row in df.iterrows():
+        graph.add_node(row['UserID'], pos(row['Latitude'], row['Longitude']))
+
+def create_edges():
+    for index, row 
 
 if __name__ == "__main__":
-    a = []
-    read_gowalla_checkins(a)
+    create_nodes()
