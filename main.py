578cde6f8c934f6a47915e901c640d3216f93a8c
Henry Tan <henrytan508@gmail.com>
2020-11-12 17:46:04 -0700

    adding all nodes in

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ import networkx as nx
 import pandas as pd
 from mpl_toolkits.basemap import Basemap
 
-path = "/Users/henry/Documents/School/Fall 2020/CSCI 5800/"
-checkin_file_path = os.path.join(path, "gowalla_checkins_usa_only_no_outliers.csv")
+path = os.path.dirname(__file__)
+checkin_file_path = os.path.join(path, "gowalla_checkins_average.csv")
 df = pd.read_csv(checkin_file_path)
 
-def 
+def create_nodes():
+    
 
 if __name__ == "__main__":
     a = []
