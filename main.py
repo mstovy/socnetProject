c6a4a30115ed1ba7acae20fa720179558a086d7d
Henry Tan <henrytan508@gmail.com>
2020-11-13 19:10:41 -0700

    removing non us edges

--- main.py
+++ main.py
@@ -1,19 +1,43 @@
 import networkx as nx
 import pandas as pd
-from mpl_toolkits.basemap import Basemap
+import matplotlib.pyplot as plt
+import os
+# from mpl_toolkits.basemap import Basemap
 
 path = os.path.dirname(__file__)
 checkin_file_path = os.path.join(path, "gowalla_checkins_average.csv")
-df = pd.read_csv(checkin_file_path)
-edges_file_path = os.path.join(path, "Gowalla_edges.txt")
+nodes_df = pd.read_csv(checkin_file_path)
+edges_file_path = os.path.join(path, "Gowalla_edges.csv")
+edges_df = pd.read_csv(edges_file_path)
 graph = nx.Graph()
+user_ids = nodes_df['UserID']
+
+# m = BaseMap(
+#     projection = 'merc',
+#     ellps = 'WGS84',
+#     llcrnrlon=-130,
+#     llcrnrlat=25,
+#     urcrnrlon=-60,
+#     urcrnrlat=50,
+#     lat_ts=0,
+#     resolution='i',
+#     suppress_ticks=True)
 
 def create_nodes():
-    for index, row in df.iterrows():
-        graph.add_node(row['UserID'], pos(row['Latitude'], row['Longitude']))
+    for index, row in nodes_df.iterrows():
+        graph.add_node(row['UserID'], pos=(row['Latitude'], row['Longitude']))
 
 def create_edges():
-    for index, row 
+    print(edges_df)
+    for index, row in edges_df.iterrows():
+        if (row['Source'] in user_ids) and  (row['Destination'] in user_ids):
+            graph.add_edge(row['Source'], row['Destination'])
+
+def draw_map():
+    nx.draw(graph)
+    plt.show()
 
 if __name__ == "__main__":
     create_nodes()
+    create_edges()
+    draw_map()
---
+++ remove_edges.py
@@ -0,0 +1,23 @@
+import os
+import pandas as pd
+
+path = os.path.dirname(__file__)
+checkin_file_path = os.path.join(path, "gowalla_checkins_average.csv")
+edges_file_path = os.path.join(path, "Gowalla_edges.csv")
+
+node_df = pd.read_csv(checkin_file_path)
+edges_df = pd.read_csv(edges_file_path)
+
+user_ids = node_df['UserID'].tolist()
+edges_in_us = {
+    'Source': [],
+    'Destination': []
+}
+
+for index, row in edges_df.iterrows():
+    if (row['Source'] in user_ids) and (row['Destination'] in user_ids):
+        edges_in_us['Source'].append(row['Source'])
+        edges_in_us['Destination'].append(row['Destination'])
+    
+df = pd.DataFrame.from_dict(edges_in_us)
+df.to_csv('gowalla_edges_only_us.csv')
