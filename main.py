afe90d5a260a124f2cd86dc00eca13a0fe2ace60
Henry Tan <henrytan508@gmail.com>
2020-11-16 16:06:08 -0700

    fixing edge file

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import networkx as nx
 import pandas as pd
-<<<<<<< HEAD
 import matplotlib.pyplot as plt
 import os
 # from mpl_toolkits.basemap import Basemap
@@ -8,7 +7,7 @@ import os
 path = os.path.dirname(__file__)
 checkin_file_path = os.path.join(path, "gowalla_checkins_average.csv")
 nodes_df = pd.read_csv(checkin_file_path)
-edges_file_path = os.path.join(path, "Gowalla_edges.csv")
+edges_file_path = os.path.join(path, "gowalla_edges_only_us.csv")
 edges_df = pd.read_csv(edges_file_path)
 graph = nx.Graph()
 user_ids = nodes_df['UserID']
@@ -29,10 +28,8 @@ def create_nodes():
         graph.add_node(row['UserID'], pos=(row['Latitude'], row['Longitude']))
 
 def create_edges():
-    print(edges_df)
     for index, row in edges_df.iterrows():
-        if (row['Source'] in user_ids) and  (row['Destination'] in user_ids):
-            graph.add_edge(row['Source'], row['Destination'])
+        graph.add_edge(row['Source'], row['Destination'])
 
 def draw_map():
     nx.draw(graph)
@@ -42,16 +39,3 @@ if __name__ == "__main__":
     create_nodes()
     create_edges()
     draw_map()
-=======
-from mpl_toolkits.basemap import Basemap
-
-path = "/Users/henry/Documents/School/Fall 2020/CSCI 5800/"
-checkin_file_path = os.path.join(path, "gowalla_checkins_usa_only_no_outliers.csv")
-df = pd.read_csv(checkin_file_path)
-
-def 
-
-if __name__ == "__main__":
-    a = []
-    read_gowalla_checkins(a)
->>>>>>> acc98e0cab1a0439857ee77398630737196f76ff
