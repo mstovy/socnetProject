2b68dd272d41abf4cb43a343d3422e6dd8d73e75
Henry Tan <henrytan508@gmail.com>
2020-11-24 18:58:29 -0700

    large community small community pages

--- community.py
+++ community.py
@@ -5,9 +5,13 @@ import networkx as nx
 path = os.path.dirname(__file__)
 checkin_file_path = os.path.join(path, "gowalla_checkins_average.csv")
 nodes_df = pd.read_csv(checkin_file_path)
-edges_file_path = os.path.join(path, "gowalla_removed_edges_1_5.csv")
+edges_file_path = os.path.join(path, "gowalla_removed_edges_1_2.csv")
 edges_df = pd.read_csv(edges_file_path)
 graph = nx.Graph()
+geocodes = {
+    'Latitude': [],
+    'Longitude': [],
+}
 
 def create_nodes():
     for index, row in nodes_df.iterrows():
@@ -18,12 +22,31 @@ def create_edges():
         graph.add_edge(row['Source'], row['Destination'])
 
 def communities():
-    response = [community for community in nx.connected_components(graph) if len(community) > 1]
-    return response
+    communities = [community for community in nx.connected_components(graph) if len(community) > 5]
+    communities.sort(key=len, reverse=True)
+    return communities
+
+def get_geocodes(node):
+    node_long = pd.to_numeric(nodes_df.loc[nodes_df['UserID']==node]['Longitude']).to_numpy()[0]
+    node_lat = pd.to_numeric(nodes_df.loc[nodes_df['UserID']==node]['Latitude']).to_numpy()[0]
+
+    geocodes['Latitude'].append(node_lat)
+    geocodes['Longitude'].append(node_long)
 
 if __name__ == "__main__":
     create_nodes()
     create_edges()
+    i = 0
     for community in communities():
-        print(community)
+        if i == 0:
+            i+=1
+            continue
+        if i == 20:
+            break
+        for node in community:
+            get_geocodes(node)
+        i+=1
+
+    df = pd.DataFrame.from_dict(geocodes)
+    df.to_csv("large_community_geocodes.csv", index=False)
 
